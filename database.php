727248968eb0192154c1d4cdcb2b331927ab423c
Project_Wolf_X <148853348+Wolfie2510@users.noreply.github.com>
2024-05-30 18:32:50 +0200

    Add files via upload

--- database.php
+++ database.php
@@ -4,4 +4,28 @@ $servername = "ITECA project";
 $username = "root";
 $password = "Project@251002";
 $dbname = "tracker_db";
-$conn = new mysqli($servername, $username, $password,
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+    die("". $conn->connect_error);
+}
+
+require_once 'database.php';
+
+// Query to fetch users
+$sql = "SELECT * FROM users";
+$result = $conn->query($sql);
+
+// Display user data
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        echo "User ID: " . $row["user_id"] . "<br>";
+        echo "Username: " . $row["username"] . "<br>";
+        echo "Email: " . $row["email"] . "<br>";
+        echo "<br>";
+    }
+} else {
+    echo "No users found.";
+}
+
+// Close the database connection
+$conn->close();
